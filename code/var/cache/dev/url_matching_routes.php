cb84e309e17ef772da97b68c6302aa6d8a9cf4e1
audriusmacbook <audrius.ivko@gmail.com>
2023-04-04 08:48:40 +0300

    create  article_edit end

--- code/var/cache/dev/url_matching_routes.php
+++ code/var/cache/dev/url_matching_routes.php
@@ -12,13 +12,18 @@ return [
     ],
     [ // $regexpList
         0 => '{^(?'
-                .'|/article/([^/]++)(*:24)'
-                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:59)'
+                .'|/article/([^/]++)(?'
+                    .'|(*:27)'
+                .')'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:63)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
-        24 => [[['_route' => 'article_view', '_controller' => 'App\\Controller\\ViewController::view'], ['id'], null, null, false, true, null]],
-        59 => [
+        27 => [
+            [['_route' => 'article_edit', '_controller' => 'App\\Controller\\EditController::edit'], ['id'], null, null, false, true, null],
+            [['_route' => 'article_view', '_controller' => 'App\\Controller\\ViewController::view'], ['id'], null, null, false, true, null],
+        ],
+        63 => [
             [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
