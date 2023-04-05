1cea8a839672a2782e56ce5e6c89351c706aaa7d
audriusmacbook <audrius.ivko@gmail.com>
2023-04-05 09:31:00 +0300

    Say Hello FormController!

--- code/src/Controller/FormController.php
+++ code/src/Controller/FormController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\BlogUpdateType;
 
 class FormController extends AbstractController
 {
--- code/var/cache/dev/twig/80/803563170d3a4c532f190016d58f7f01.php
+++ code/var/cache/dev/twig/80/803563170d3a4c532f190016d58f7f01.php
@@ -30,6 +30,7 @@ class __TwigTemplate_df1eb86cf6f94d26c0da31a0fc3a0c09 extends Template
             'title' => [$this, 'block_title'],
             'stylesheets' => [$this, 'block_stylesheets'],
             'main' => [$this, 'block_main'],
+            'body' => [$this, 'block_body'],
             'footer_js' => [$this, 'block_footer_js'],
         ];
     }
@@ -43,29 +44,35 @@ class __TwigTemplate_df1eb86cf6f94d26c0da31a0fc3a0c09 extends Template
         // line 1
         echo "<!DOCTYPE html>
 <html lang=\"en\">
-    <head>
-        <meta charset=\"UTF-8\">
-        <title>";
-        // line 5
-        $this->displayBlock('title', $context, $blocks);
-        echo "</title>
-        ";
+\t<head>
+\t\t<meta charset=\"UTF-8\">
+\t\t<title>
+\t\t\t";
         // line 6
-        $this->displayBlock('stylesheets', $context, $blocks);
+        $this->displayBlock('title', $context, $blocks);
+        // line 8
+        echo "\t\t</title>
+\t\t";
         // line 9
-        echo "    </head>
-    <body>
-
-    ";
+        $this->displayBlock('stylesheets', $context, $blocks);
         // line 12
+        echo "\t</head>
+\t<body>
+
+\t\t";
+        // line 15
         $this->displayBlock('main', $context, $blocks);
-        // line 13
-        echo "
-    ";
-        // line 14
-        $this->displayBlock('footer_js', $context, $blocks);
+        // line 16
+        echo "\t\t";
+        $this->displayBlock('body', $context, $blocks);
         // line 17
-        echo "    </body>
+        echo "
+
+\t\t";
+        // line 19
+        $this->displayBlock('footer_js', $context, $blocks);
+        // line 22
+        echo "\t</body>
 </html>
 ";
         
@@ -73,35 +80,36 @@ class __TwigTemplate_df1eb86cf6f94d26c0da31a0fc3a0c09 extends Template
 
     }
 
-    // line 5
+    // line 6
     public function block_title($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
 
-        echo "Welcome!";
+        echo "Welcome!
+\t\t\t";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
     }
 
-    // line 6
+    // line 9
     public function block_stylesheets($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
 
-        // line 7
-        echo "            <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD\" crossorigin=\"anonymous\">
-        ";
+        // line 10
+        echo "\t\t\t<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD\" crossorigin=\"anonymous\">
+\t\t";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
     }
 
-    // line 12
+    // line 15
     public function block_main($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -113,16 +121,28 @@ class __TwigTemplate_df1eb86cf6f94d26c0da31a0fc3a0c09 extends Template
 
     }
 
-    // line 14
+    // line 16
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    // line 19
     public function block_footer_js($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "footer_js"));
 
-        // line 15
-        echo "        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN\" crossorigin=\"anonymous\"></script>
-    ";
+        // line 20
+        echo "\t\t\t<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN\" crossorigin=\"anonymous\"></script>
+\t\t";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
@@ -135,28 +155,33 @@ class __TwigTemplate_df1eb86cf6f94d26c0da31a0fc3a0c09 extends Template
 
     public function getDebugInfo()
     {
-        return array (  124 => 15,  117 => 14,  105 => 12,  97 => 7,  90 => 6,  77 => 5,  68 => 17,  66 => 14,  63 => 13,  61 => 12,  56 => 9,  54 => 6,  50 => 5,  44 => 1,);
+        return array (  144 => 20,  137 => 19,  125 => 16,  113 => 15,  105 => 10,  98 => 9,  84 => 6,  75 => 22,  73 => 19,  69 => 17,  66 => 16,  64 => 15,  59 => 12,  57 => 9,  54 => 8,  52 => 6,  45 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Source("<!DOCTYPE html>
 <html lang=\"en\">
-    <head>
-        <meta charset=\"UTF-8\">
-        <title>{% block title %}Welcome!{% endblock %}</title>
-        {% block stylesheets %}
-            <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD\" crossorigin=\"anonymous\">
-        {% endblock %}
-    </head>
-    <body>
+\t<head>
+\t\t<meta charset=\"UTF-8\">
+\t\t<title>
+\t\t\t{% block title %}Welcome!
+\t\t\t{% endblock %}
+\t\t</title>
+\t\t{% block stylesheets %}
+\t\t\t<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD\" crossorigin=\"anonymous\">
+\t\t{% endblock %}
+\t</head>
+\t<body>
 
-    {% block main %}{% endblock %}
+\t\t{% block main %}{% endblock %}
+\t\t{% block body %}{% endblock %}
 
-    {% block footer_js %}
-        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN\" crossorigin=\"anonymous\"></script>
-    {% endblock %}
-    </body>
+
+\t\t{% block footer_js %}
+\t\t\t<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN\" crossorigin=\"anonymous\"></script>
+\t\t{% endblock %}
+\t</body>
 </html>
 ", "base.html.twig", "/code/templates/base.html.twig");
     }
