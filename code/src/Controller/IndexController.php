5b3cf37d3932f93e68a7de3e94a1763b44d4b4e2
audriusmacbook <audrius.ivko@gmail.com>
2023-04-05 21:57:31 +0300

    AT

--- code/src/Controller/IndexController.php
+++ code/src/Controller/IndexController.php
@@ -6,14 +6,19 @@ use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Collections\Criteria;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function list(ArticleRepository $articleRepository): Response
     {
+        //1680712726
+        $ss=$articleRepository->findAll();
+
+    
         return $this->render('pages/index.html.twig', [
-            'articles' => $articleRepository->findAll(),
+            'articles' => $ss,
         ]);
     }
 }
