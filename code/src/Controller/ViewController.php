318b52087c6c61b60dd82f3e8e0bdbe30667728c
audriusmacbook <audrius.ivko@gmail.com>
2023-04-06 12:20:29 +0300

    Implement "X" minutes to read functionality

--- code/src/Controller/ViewController.php
+++ code/src/Controller/ViewController.php
@@ -12,8 +12,21 @@ class ViewController extends AbstractController
     #[Route('/article/{id}', name: 'article_view')]
     public function view(Article $article): Response
     {
+        $srting = $article->getText('text');    
+        $count=str_word_count($srting, 1);
+        $qty=0;
+        foreach ($count as $counts) {
+            if(strlen($counts)>3) {
+                $qty++;
+            };
+        }
+        $mins=round(($qty/200),2);
+        
         return $this->render('pages/view.html.twig', [
             'article' => $article,
+            // 'stringCount'=>$count,
+            'srting'=>$mins,
+
         ]);
     }
 }
